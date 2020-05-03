1b107d40061f7d71d4853ba11c4d477350ddf967
Capotasto <rocker519804@gmail.com>
2020-05-03 22:31:44 +0900

    Added shuffle dice function

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,67 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(DicePage());
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void shuffleDice() {
+    setState(() {
+      leftDiceNumber = getRandomNumber();
+      rightDiceNumber = getRandomNumber();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset(
+                    'images/dice$leftDiceNumber.png',
+                  ),
+                  onPressed: () {
+                    shuffleDice();
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset(
+                    'images/dice$rightDiceNumber.png',
+                  ),
+                  onPressed: () {
+                    shuffleDice();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
 
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+int getRandomNumber() {
+  return new Random().nextInt(6) + 1; // 1 to 6
 }
